b38ecc19be1dcf5002c2ff9c1c44ca719e4482e8
Benjamin Pichon <benpichon@hotmail.fr>
2022-05-20 15:48:43 +0200

    UPDATE Homepage css with tailwind class

--- src/components/SpaceButton.vue
+++ src/components/SpaceButton.vue
@@ -1,9 +1,11 @@
 <template>
-	<router-link :to="{ name: link }">
-		<div class="button">
-			{{ text.toUpperCase() }} 
-		</div>
-	</router-link>
+<router-link :to="{ name: link }">
+    <div class="
+      bg-space-dark-blue hover:bg-white hover:text-space-blue text-center rounded-full font-bold py-2 px-1 text-xs xl:text-base cursor-pointer shadow-space"
+    >
+        {{ text.toUpperCase() }}
+    </div>
+</router-link>
 </template>
 
 <script>
@@ -17,27 +19,6 @@
 </script>
 
 <style scoped>
-    .button {
-        text-align: center;
-        background-color: #090B31;
-        border-radius: 30px;
-        font-weight: 700;
-        padding: 12px 0;
-        box-shadow: 0px 0px 50px 5px rgba(48,181,255,0.8);
-        transition: 0.25s;
-        cursor: pointer;
-        color: #fff;
-    }
-    .button:hover {
-        background-color: #fff;
-        color: #4D2A9C;
-        cursor: pointer;
-        transition: 0.25s;
-    }
-    .btn-small {
-        padding: 3px 0;
-        font-size: 1rem;
-    }
     a {
     color: white;
     text-decoration: none;
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,17 @@ module.exports = {
   ],
   theme: {
     extend: {},
+    colors: {
+      'white' : '#fff',
+      'space-dark-blue' : '#090B31',
+      'space-blue' : '#4D2A9C'
+    },
+    boxShadow: {
+      'space': '0px 0px 50px 5px rgba(48,181,255,0.8)',
+    },
+    animation: {
+      'spin-slow': 'spin 120s linear infinite',
+    }
   },
   plugins: [],
 }
