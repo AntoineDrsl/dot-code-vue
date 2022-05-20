ca28bfa7316557bb4bce9c9bbecf99c926f9f8e7
Benjamin Pichon <benpichon@hotmail.fr>
2022-05-20 16:50:35 +0200

    UPDATE Pseudo vue with tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,13 +8,17 @@ module.exports = {
     colors: {
       'white' : '#fff',
       'space-dark-blue' : '#090B31',
-      'space-blue' : '#4D2A9C'
+      'space-blue' : '#4D2A9C',
+      'space-green' : '#42b983'
     },
     boxShadow: {
       'space': '0px 0px 50px 5px rgba(48,181,255,0.8)',
     },
     animation: {
       'spin-slow': 'spin 120s linear infinite',
+    },
+    zIndex: {
+      'negative' : '-1'
     }
   },
   plugins: [],
