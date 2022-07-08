d53c09372bf823e2954add01fc8072d2a2d428c3
Brazzolotto Nicolas <nicolas.brazzolotto@edu.devinci.fr>
2022-07-08 21:23:50 +0200

    :construction: add particles animation

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,7 @@
 <template>
 	<div id="app">
+
+    <Particles />
 		<router-view />
 	</div>
 </template>
@@ -9,3 +11,9 @@
     color: #FFF;
   }
 </style>
+<script>
+import Particles from "./components/Particles/Particles";
+export default {
+  components: {Particles}
+}
+</script>
