bdd4a7bed13c0f66d0db4e726044cf04a471b0ba
AntoineDrsl <antoine.durussel@gmail.com>
2022-07-16 18:12:00 +0200

    :bug: FIX win & lose button view

--- src/components/Room/Win.vue
+++ src/components/Room/Win.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="win">
-    <a class="text-white no-underline" href="">Retour dans le CyberSpace</a>
+    <a class="text-white no-underline" href="/">Retour dans le CyberSpace</a>
     <div class="relative bg-white rounded-lg p-2 text-black text-shadow">
       <div class="geminis">
         <h2 class="text-5xl xl:text-8xl">Bravo !</h2>
@@ -17,9 +17,16 @@
     <div class="flex p-1.5 ml-1 space-x-4">
       <button 
         class="w-full bg-space-green text-center rounded-full border-none relative text-white text-xl font-bold px-4 py-2 transition duration-150 hover:bg-space-darker-green shadow-space"
-        @click="$router.push('/')">Evidemment !</button>
-      <button class="w-full bg-space-red text-center rounded-full border-none relative text-white text-xl font-bold px-4 py-2 transition duration-150 hover:bg-space-darker-red shadow-space"
-        @click="$router.push('/')">Non, j'ai eu ma dose</button>
+        @click="$router.push('/')"
+      >
+        Evidemment !
+      </button>
+      <button 
+        class="w-full bg-space-red text-center rounded-full border-none relative text-white text-xl font-bold px-4 py-2 transition duration-150 hover:bg-space-darker-red shadow-space"
+        @click="$router.push('/')"
+      >
+        Non, j'ai eu ma dose
+      </button>
     </div>
   </div>
 </template>
