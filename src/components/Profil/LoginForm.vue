d4d630dd8c82d85a5a4767187b683e1906082654
Brazzolotto Nicolas <nicolas.brazzolotto@edu.devinci.fr>
2022-07-21 22:48:50 +0200

    :construction: Update register form

--- src/components/Profil/LoginForm.vue
+++ src/components/Profil/LoginForm.vue
@@ -3,12 +3,15 @@
 		id="LoginForm"
 		class="bg-white text-black p-4 rounded-3xl"
 	>
-		<div class="block mt-2 mb-10">
+        <div class="error-box" v-if="error">
+            <span class="text-space-darker-red">Email ou mot de passe incorrect</span>
+        </div>
+		<div class="block mt-4 mb-10">
 			<input
 				id="id-input"
 				v-model="email"
 				type="text"
-				class="w-full mt-4 py-2 px-4 rounded-full border-none bg-space-dark-blue text-white"
+				class="w-full py-2 px-4 rounded-full border-none bg-space-dark-blue text-white"
 				placeholder="Email"
 			>
 			<input
@@ -21,12 +24,23 @@
 		</div>
 		<div class="w-full flex justify-center">
 			<button
-				class="w-4/5 mt-2 mb-4 text-center rounded-full font-bold text-white px-4 py-2 cursor-pointer bg-space-dark-blue hover:bg-white hover:text-space-dark-blue"
+                type="button"
+				class="w-4/5 mt-2 mb-4 text-center rounded-full font-bold text-white px-4 py-2 cursor-pointer bg-space-dark-blue transition duration-150 btn-hover"
 				@click="onLogin"
+                :disabled="loading"
+                :class="{
+					'bg-gray-300 cursor-not-allowed' : loading
+				}"
 			>
 				Se connecter
 			</button>
 		</div>
+        <p class="text-xs mt-2 text-center">
+            Pas encore inscrit ?
+            <router-link :to="{name: 'register'}" class="font-bold no-underline cursor-pointer">
+                    S'enregistrer
+            </router-link>
+        </p>
 	</div>
 </template>
 
@@ -36,30 +50,45 @@ import axios from "axios";
 export default {
     name: "LoginForm",
     data() {
-      return {
-        email: "",
-        password: "",
-      }
+        return {
+            email: "",
+            password: "",
+            error: false,
+            loading: false
+        }
     },
     methods: {
-      onLogin() {
-        const userId = localStorage.getItem('user');
+        async onLogin() {
+            this.error = false;
+            this.loading = true;
+            const userId = localStorage.getItem('user');
 
-        axios.post(process.env.VUE_APP_API_URL + `auth/login?id=${userId}`, {
-          email: this.email.toLocaleLowerCase(),
-          password: this.password
-        }).then((response) => {
-          if ('id' in response.data) {
-            localStorage.setItem('token', response.data.access_token);
-            localStorage.setItem('user', response.data.id);
-            sessionStorage.setItem('username', response.data.username);
-            return this.$router.push({name: 'home'});
-          }
-        })
-      }
+            await axios.post(process.env.VUE_APP_API_URL + `auth/login?id=${userId}`, {
+                  email: this.email.toLocaleLowerCase(),
+                  password: this.password
+            }).then((response) => {
+                  if ('id' in response.data) {
+                    localStorage.setItem('token', response.data.access_token);
+                    localStorage.setItem('user', response.data.id);
+                    sessionStorage.setItem('username', response.data.username);
+                    return this.$router.push({name: 'home'});
+                  }
+            }).catch((error) => this.error = true);
+
+            this.loading = false;
+        }
     }
 }
 </script>
 
 <style scoped>
+input, button {
+    box-shadow: 0 0 20px rgba(77, 42, 156, 0.43);
+}
+
+.btn-hover:hover {
+    /* OBLIGER LE SURCLASSER AVEC !IMPORTANT CAR LE TEXT-WHITE DE TAILWIND MET AUSSI !IMPORTANT... */
+    color: #090B31 !important;
+    background-color: white;
+}
 </style>
--- src/views/Profile/Login.vue
+++ src/views/Profile/Login.vue
@@ -1,91 +1,31 @@
 <template>
-	<div
-		id="Login"
-		class="px-20">
-
-        <SimpleTopBar />
-
-        <div class="grid grid-cols-2 gap-x-16 mt-10">
-            <div class="w-full flex flex-column items-end space-y-7">
-                <div>
-                    <!-- C'est très sale comment c'est fait
-                    mais je ne sais pas comment surpasser le router-link avec un svg-->
-                    <router-link :to="{name: 'home'}">
-                        <div class="flex items-center text-white cursor-pointer font-bold">
-                            <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                class="h-5 w-5"
-                                viewBox="0 0 20 20"
-                                fill="currentColor"
-                            >
-                                <path
-                                    fill-rule="evenodd"
-                                    d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z"
-                                    clip-rule="evenodd"
-                                />
-                            </svg>
-                            RETOUR DANS LE CYBERSPACE
-                        </div>
-                    </router-link>
-<!--                    <a-->
-<!--                        class="flex items-center text-white no-underline cursor-pointer font-bold"-->
-<!--                        href="/"-->
-<!--                    >-->
-<!--                        -->
-<!--                    </a>-->
-                </div>
-                <div>
-                    <p class="geminis text-9xl text-shadow text-end">
-                        WELCOME BACK !
-                    </p>
-                </div>
-                <div>
-                    <ul class="flex w-full list-none font-black text-space-green p-0 space-x-12">
-                        <li>
-                            <span>FOLLOW US</span>
-                        </li>
-                        <li>
-                            <a href="#"><img src="@/assets/images/twitter.png"></a>
-                        </li>
-                        <li class="">
-                            <a href="#"><img src="@/assets/images/facebook.png"></a>
-                        </li>
-                        <li class="">
-                            <a href="#"><img src="@/assets/images/linkedin.png"></a>
-                        </li>
-                        <li class="">
-                            <a href="#"><img src="@/assets/images/discord.png"></a>
-                        </li>
-                        <li class="">
-                            <a href="#"><img src="@/assets/images/twitch.png"></a>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-            <div class="2xl:w-3/4">
-                <p class="geminis text-space-green text-3xl text-center">SE CONNECTER</p>
-                <LoginForm />
-                <ConnexionButton />
-            </div>
-        </div>
-	</div>
+	<AuthentificationLayout>
+        <template #background-image>
+            <img src="~@/assets/images/planet-ring.png">
+        </template>
+        <template #text-page>
+            <p class="geminis text-9xl text-shadow text-end">
+                WELCOME BACK !
+            </p>
+        </template>
+        <template #form>
+            <p class="geminis text-space-green text-3xl text-center">SE CONNECTER</p>
+            <LoginForm />
+            <ConnexionButton />
+        </template>
+	</AuthentificationLayout>
 </template>
 
 <script>
-import LoginForm from "../../components/Profil/LoginForm.vue";
-import ConnexionButton from "../../components/Profil/ConnexionButton.vue";
-import SimpleTopBar from "../../layouts/partials/other-layout/SimpleTopBar";
+import AuthentificationLayout from "../../layouts/partials/authentification-layout/AuthentificationLayout";
+import LoginForm from "../../components/Profil/LoginForm";
+import ConnexionButton from "../../components/Profil/ConnexionButton";
 export default {
     name : "Login",
     components: {
-        SimpleTopBar,
+        ConnexionButton,
         LoginForm,
-        ConnexionButton
+        AuthentificationLayout,
     }
 }
 </script>
-<style scoped>
-a {
-    text-decoration: none;
-}
-</style>
