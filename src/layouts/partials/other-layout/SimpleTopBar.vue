7fdac3a59aa9c0e3249a8c8ac1ee1369f794e593
Brazzolotto Nicolas <nicolas.brazzolotto@edu.devinci.fr>
2022-07-21 21:56:16 +0200

    :construction: UPDATE Login.vue css

---
+++ src/layouts/partials/other-layout/SimpleTopBar.vue
@@ -0,0 +1,10 @@
+<template>
+    <div class="w-full flex justify-center mt-2">
+        <img
+            class="w-24"
+            src="@/assets/images/SpaceCodeGreenLogo.png"
+            alt="space_code_logo"
+        >
+    </div>
+
+</template>
--- src/views/Profile/Login.vue
+++ src/views/Profile/Login.vue
@@ -1,91 +1,91 @@
 <template>
 	<div
 		id="Login"
-		class="h-screen"
-	>
-		<div class="w-full flex justify-center mt-2">
-			<img
-				class="w-24"
-				src="@/assets/images/SpaceCodeGreenLogo.png"
-			>
-		</div>
-		<div class="w-full mt-5 flex justify-center relative z-20">
-            <SpaceButton
-                class="w-2/6"
-                text="Retour à l'accueil"
-                link="home"
-            />
-			<SpaceButton
-				class="w-2/6 ml-14"
-				text="Se créer un compte"
-				link="register"
-			/>
-		</div>
-		<img
-			class="w-1/2 -mt-44  -ml-10 relative z-10"
-			src="@/assets/images/planet-ring.png"
-		>
-		<div class="w-full -mt-96 flex relative z-30">
-			<div class="w-1/2 ml-40">
-				<a
-					class="flex items-center text-white no-underline cursor-pointer"
-					href="/"
-				>
-					<svg
-						xmlns="http://www.w3.org/2000/svg"
-						class="h-5 w-5"
-						viewBox="0 0 20 20"
-						fill="currentColor"
-					>
-						<path
-							fill-rule="evenodd"
-							d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z"
-							clip-rule="evenodd"
-						/>
-					</svg>
-					Retour dans le cyberspace
-				</a>
-				<h1 class="text-shadow mt-4 lg:text-7xl xl:text-9xl geminis geminis">
-					Welcome<br>Back
-				</h1>
-				<ul class="flex w-full list-none font-bold text-space-green">
-					<li class="ml-6">
-						FOLLOW US
-					</li>
-					<li class="ml-16">
-						<a href="#"><img src="@/assets/images/twitter.png"></a>
-					</li>
-					<li class="ml-6">
-						<a href="#"><img src="@/assets/images/facebook.png"></a>
-					</li>
-					<li class="ml-6">
-						<a href="#"><img src="@/assets/images/linkedin.png"></a>
-					</li>
-					<li class="ml-6">
-						<a href="#"><img src="@/assets/images/discord.png"></a>
-					</li>
-					<li class="ml-6">
-						<a href="#"><img src="@/assets/images/twitch.png"></a>
-					</li>
-				</ul>
-			</div>
-			<div class="w-2/6 -mt-10 -ml-32">
-				<LoginForm />
-				<ConnexionButton />
-			</div>
-		</div>
+		class="px-20">
+
+        <SimpleTopBar />
+
+        <div class="grid grid-cols-2 gap-x-16 mt-10">
+            <div class="w-full flex flex-column items-end space-y-7">
+                <div>
+                    <!-- C'est très sale comment c'est fait
+                    mais je ne sais pas comment surpasser le router-link avec un svg-->
+                    <router-link :to="{name: 'home'}">
+                        <div class="flex items-center text-white cursor-pointer font-bold">
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                class="h-5 w-5"
+                                viewBox="0 0 20 20"
+                                fill="currentColor"
+                            >
+                                <path
+                                    fill-rule="evenodd"
+                                    d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z"
+                                    clip-rule="evenodd"
+                                />
+                            </svg>
+                            RETOUR DANS LE CYBERSPACE
+                        </div>
+                    </router-link>
+<!--                    <a-->
+<!--                        class="flex items-center text-white no-underline cursor-pointer font-bold"-->
+<!--                        href="/"-->
+<!--                    >-->
+<!--                        -->
+<!--                    </a>-->
+                </div>
+                <div>
+                    <p class="geminis text-9xl text-shadow text-end">
+                        WELCOME BACK !
+                    </p>
+                </div>
+                <div>
+                    <ul class="flex w-full list-none font-black text-space-green p-0 space-x-12">
+                        <li>
+                            <span>FOLLOW US</span>
+                        </li>
+                        <li>
+                            <a href="#"><img src="@/assets/images/twitter.png"></a>
+                        </li>
+                        <li class="">
+                            <a href="#"><img src="@/assets/images/facebook.png"></a>
+                        </li>
+                        <li class="">
+                            <a href="#"><img src="@/assets/images/linkedin.png"></a>
+                        </li>
+                        <li class="">
+                            <a href="#"><img src="@/assets/images/discord.png"></a>
+                        </li>
+                        <li class="">
+                            <a href="#"><img src="@/assets/images/twitch.png"></a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div class="2xl:w-3/4">
+                <p class="geminis text-space-green text-3xl text-center">SE CONNECTER</p>
+                <LoginForm />
+                <ConnexionButton />
+            </div>
+        </div>
 	</div>
 </template>
 
 <script>
 import LoginForm from "../../components/Profil/LoginForm.vue";
-import SpaceButton from "../../components/SpaceButton.vue";
 import ConnexionButton from "../../components/Profil/ConnexionButton.vue";
+import SimpleTopBar from "../../layouts/partials/other-layout/SimpleTopBar";
 export default {
     name : "Login",
-  components: { LoginForm, SpaceButton, ConnexionButton }
+    components: {
+        SimpleTopBar,
+        LoginForm,
+        ConnexionButton
+    }
 }
 </script>
-
 <style scoped>
+a {
+    text-decoration: none;
+}
 </style>
